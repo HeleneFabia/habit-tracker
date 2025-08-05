9328170d0a682a3e4589dd9080b40e221b174ebe
Helene Kortschak <helene.kortschak@gmail.com>
2025-08-05 17:24:36 +0200

    Add files via upload

---
+++ habit tracker/habit_trackerApp.swift
@@ -0,0 +1,16 @@
+//
+//  habit_trackerApp.swift
+//  habit tracker
+//
+//  Created by Helene on 04.08.25.
+//
+
+import SwiftUI
+
+struct habit_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
