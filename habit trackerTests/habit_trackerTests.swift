01973ec036821b36dae7a0f4a119a3e8dca252b8
Helene Kortschak <helene.kortschak@gmail.com>
2025-08-05 17:27:21 +0200

    Add files via upload

---
+++ habit trackerTests/habit_trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  habit_trackerTests.swift
+//  habit trackerTests
+//
+//  Created by Helene on 04.08.25.
+//
+
+import Testing
+@testable import habit_tracker
+
+struct habit_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
